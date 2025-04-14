da642c55479245ae0b92e06e7ce2196dd7799609
Behnam Dakermanji <dakermanji@gmail.com>
2025-04-14 14:45:45 -0400

    Refactor follow notification handler using switch statement
    ** utils/friends/followNotificationsHelper.js
    Replaced multiple if conditions with a switch(action) structure for better readability and maintainability in handleFollowNotificationAction.
    Added a default case to catch unsupported actions with a clear error message.

--- utils/friends/followNotificationsHelper.js
+++ utils/friends/followNotificationsHelper.js
@@ -4,7 +4,7 @@ import FollowNotification from '../../models/FollowNotification.js';
 import FollowRequest from '../../models/FollowRequest.js';
 import Follow from '../../models/Follow.js';
 import { blockUser, unfollowUser, unfollowBothUsers } from './friendsHelper.js';
-// General function to handle notification actions
+
 export const handleFollowNotificationAction = async (
 	notificationId,
 	userId,
@@ -26,6 +26,7 @@ export const handleFollowNotificationAction = async (
 		};
 	}
 
+	// Clean up follow request if applicable
 	if (notification.type === 'follow_request') {
 		await FollowRequest.deleteFollowRequest(
 			notification.sender_id,
@@ -33,38 +34,54 @@ export const handleFollowNotificationAction = async (
 		);
 	}
 
+	// Always delete the notification
 	await FollowNotification.deleteNotification(notificationId);
 
-	if (action === 'accept') {
-		await Follow.createFollow(notification.sender_id, notification.user_id);
-		await FollowNotification.createFollowNotification(
-			notification.sender_id,
-			notification.user_id,
-			'follow_accepted'
-		);
-	}
+	// Handle specific actions
+	switch (action) {
+		case 'accept':
+			await Follow.createFollow(
+				notification.sender_id,
+				notification.user_id
+			);
+			await FollowNotification.createFollowNotification(
+				notification.sender_id,
+				notification.user_id,
+				'follow_accepted'
+			);
+			break;
 
-	if (action === 'follow_back') {
-		await Follow.createMutualFollow(
-			notification.sender_id,
-			notification.user_id
-		);
-		await FollowNotification.createFollowNotification(
-			notification.sender_id,
-			notification.user_id,
-			'follow_back'
-		);
-	}
+		case 'follow_back':
+			await Follow.createMutualFollow(
+				notification.sender_id,
+				notification.user_id
+			);
+			await FollowNotification.createFollowNotification(
+				notification.sender_id,
+				notification.user_id,
+				'follow_back'
+			);
+			break;
 
-	if (action === 'block') {
-		await blockUser(notification.user_id, notification.sender_id);
-	}
+		case 'block':
+			await blockUser(notification.user_id, notification.sender_id);
+			break;
 
-	if (action === 'unfollow') {
-		await unfollowUser(notification.user_id, notification.sender_id);
-	}
+		case 'unfollow':
+			await unfollowUser(notification.user_id, notification.sender_id);
+			break;
 
-	if (action === 'unfollow_both') {
-		await unfollowBothUsers(notification.user_id, notification.sender_id);
+		case 'unfollow_both':
+			await unfollowBothUsers(
+				notification.user_id,
+				notification.sender_id
+			);
+			break;
+
+		default:
+			throw {
+				status: 400,
+				message: `Unsupported action: ${action}`,
+			};
 	}
 };
